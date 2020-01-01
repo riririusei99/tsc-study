170722daf6803cf5e59b341425ffb0f056fa2ded
Ryusei Namai <riry1313@gmail.com>
2020-01-01 14:14:42 +0900

    study declare function

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 "use strict";
-// 引数についても型を引数を指定する必要がある
 function multiply(value1, value2) {
     return value1 * value2;
 }
-console.log(multiply(23, 12));
+function sayHello() {
+    console.log("Hello!");
+}
+var myFunctionType;
+myFunctionType = multiply;
+console.log(myFunctionType(23, 89));
--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
-// 引数についても型を引数を指定する必要がある
+
 function multiply(value1: number, value2: number): number{
   return value1 * value2;
 }
-console.log(multiply(23, 12)); 
+
+function sayHello(): void{
+  console.log("Hello!");
+}
+
+let myFunctionType: (val1: number, val2: number) => number;
+myFunctionType = multiply;
+console.log(myFunctionType(23, 89));

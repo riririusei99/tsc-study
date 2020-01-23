9c88b3434d1cff573558041d3506ce81ea149784
Ryusei Namai <riry1313@gmail.com>
2020-01-24 08:46:19 +0900

    study method

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ function greet(person) {
 var person = {
     name: "太郎",
     age: 30,
-    hobbies: ["game", "sports"]
+    hobbies: ["game", "sports"],
+    greet: function (lastName) {
+        console.log("こんにちは、" + lastName + this.name + "!");
+    }
 };
 function changeName(person) {
     person.name = "権兵衛";
 }
-greet({ name: "太郎", age: 30 });
+// greet({name: "太郎", age: 30});
 changeName(person);
 greet(person);
+person.greet("名無しの");
--- app.ts
+++ app.ts
@@ -2,20 +2,25 @@ interface NamedPerson {
   name: string;
   age?: number;
   [prepName: string]: any;
+  greet(lastName: string): void;
 }
 
 
 function greet(person: NamedPerson): void {
   console.log("こんにちは、" + person.name + "！");
 }
-const person = {
+const person: NamedPerson = {
   name: "太郎",
   age: 30,
-  hobbies: ["game", "sports"]
+  hobbies: ["game", "sports"],
+  greet(lastName: string){
+    console.log("こんにちは、" + lastName + this.name + "!");
+  }
 };
 function changeName(person: NamedPerson){
   person.name = "権兵衛";
 }
-greet({name: "太郎", age: 30});
+// greet({name: "太郎", age: 30});
 changeName(person);
 greet(person);
+person.greet("名無しの");

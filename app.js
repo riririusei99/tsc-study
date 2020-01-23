c751a5a72790fe3fe4efa81f2010261f527c7ded
Ryusei Namai <riry1313@gmail.com>
2020-01-24 08:28:56 +0900

    study interFace

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 "use strict";
-console.log(MyMath.Circum.calcCircum(3));
-console.log(MyMath.calcRect(20, 8));
-var PI = 2.14;
-console.log(PI);
-var math = MyMath.Circum;
-console.log(math.calcCircum(3));
+function greet(person) {
+    console.log("こんにちは、" + person.name + "！");
+}
+var person = {
+    firstName: "太郎",
+    age: 30
+};
+greet(person);
--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
 
-console.log(MyMath.Circum.calcCircum(3));
-console.log(MyMath.calcRect(20,8));
-
-const PI = 2.14;
-console.log(PI);
-
-import math = MyMath.Circum;
-console.log(math.calcCircum(3));
+function greet(person: {name: string}): void {
+    console.log("こんにちは、" + person.name + "！");
+}
+const person = {
+  firstName: "太郎",
+  age: 30
+};
+greet(person);
